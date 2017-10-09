c0824ca7f2ca68d3202ba5c2dfbd9f5ac5c18bcd
sil9 <sil9tyt@gmail.com>
2017-10-09 15:50:45 +0300

    create runner class

---
+++ src/main/java/com/netcracker/lesson1/Main.java
@@ -0,0 +1,16 @@
+package com.netcracker.lesson1;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Person> persons = new ArrayList<>(Arrays.asList(new Customer("Jacky", 300),
+                new JavaDeveloper("Dmitry", 10), new KotlinDeveloper("Katy", 30)));
+        persons.forEach(Person::speak);
+        List<Developer> developers = new ArrayList<>(Arrays.asList(new JavaDeveloper(), new KotlinDeveloper()));
+        developers.forEach(Developer::writeCode);
+    }
+}
