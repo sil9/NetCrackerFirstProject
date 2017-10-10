e71f4680e9b36f0f50c44bce807537d4407f8b5a
sil9 <sil9tyt@gmail.com>
2017-10-10 04:08:51 +0300

    menu switchers commands

---
+++ src/main/java/com/netcracker/homework1/command/impl/OperationsCommand.java
@@ -0,0 +1,13 @@
+package com.netcracker.homework1.command.impl;
+
+import com.netcracker.homework1.command.Command;
+import com.netcracker.homework1.menu.MenuFactory;
+import com.netcracker.homework1.menu.MenuType;
+
+public class OperationsCommand implements Command {
+
+    @Override
+    public void execute() {
+        MenuFactory.getInstance().create(MenuType.OPERATIONS).show();
+    }
+}
---
+++ src/main/java/com/netcracker/homework1/command/impl/StartCommand.java
@@ -0,0 +1,14 @@
+package com.netcracker.homework1.command.impl;
+
+import com.netcracker.homework1.command.Command;
+import com.netcracker.homework1.menu.MenuFactory;
+import com.netcracker.homework1.menu.MenuType;
+
+
+public class StartCommand implements Command {
+
+    @Override
+    public void execute() {
+        MenuFactory.getInstance().create(MenuType.START).show();
+    }
+}
