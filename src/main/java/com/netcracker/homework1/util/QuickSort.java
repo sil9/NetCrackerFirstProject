46697d2b1f4c6329341a31ee3a5553d726150625
sil9 <sil9tyt@gmail.com>
2017-10-10 17:00:43 +0300

    write a sorting for array(QuickSort)

---
+++ src/main/java/com/netcracker/homework1/util/QuickSort.java
@@ -0,0 +1,43 @@
+package com.netcracker.homework1.util;
+
+
+public class QuickSort {
+    private int[] array ;
+
+    public QuickSort(int[] array) {
+        this.array = array;
+    }
+
+    public void quickSort() {
+        int startIndex = 0;
+        int endIndex = array.length- 1;
+        doSort(startIndex, endIndex);
+    }
+
+    private void doSort(int start, int end) {
+        if (start >= end)
+            return;
+        int i = start, j = end;
+        int cur = i - (i - j) / 2;
+        while (i < j) {
+            while (i < cur && (array[i] <= array[cur])) {
+                i++;
+            }
+            while (j > cur && (array[cur] <= array[j])) {
+                j--;
+            }
+            if (i < j) {
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+                if (i == cur)
+                    cur = j;
+                else if (j == cur)
+                    cur = i;
+            }
+        }
+        doSort(start, cur);
+        doSort(cur + 1, end);
+    }
+
+}
