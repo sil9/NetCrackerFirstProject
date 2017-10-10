d769ee82e1922c08489a8660f9f31b74e366c768
sil9 <sil9tyt@gmail.com>
2017-10-10 04:03:48 +0300

    create array(need refactor)

---
+++ src/main/java/com/netcracker/homework1/repository/ArrayOfNumbers.java
@@ -0,0 +1,18 @@
+package com.netcracker.homework1.repository;
+
+
+public class ArrayOfNumbers {
+    private static int[] array = {7, 8, 10, 20, 30};
+
+//    public static void init(int size){
+//        array = new int[size];
+//    }
+//
+//    public static void addNumber(int position, int number){
+//        array[position] = number;
+//    }
+
+    public static int[] getArray() {
+        return array;
+    }
+}
