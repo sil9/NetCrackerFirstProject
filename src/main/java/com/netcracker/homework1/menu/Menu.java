96e42eeb372d75b3d138d87dbdbe2c8f4094b8fa
sil9 <sil9tyt@gmail.com>
2017-10-11 03:00:51 +0300

    menu factory is ready

--- src/main/java/com/netcracker/homework1/menu/Menu.java
+++ src/main/java/com/netcracker/homework1/menu/Menu.java
@@ -3,27 +3,42 @@ package com.netcracker.homework1.menu;
 
 import com.netcracker.homework1.command.factory.CommandFactory;
 import com.netcracker.homework1.command.factory.CommandType;
+import com.netcracker.homework1.memory.InputValues;
 import com.netcracker.homework1.util.ConsoleReader;
 import javafx.util.Pair;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Menu {
     private List<Pair<String, CommandType>> commands = new ArrayList<>();
     private ConsoleReader consoleReader = new ConsoleReader();
+    private boolean choice = true;
+    private boolean exit = false;
+    private Menu previousMenu;
 
     public void show() {
-        int i = 1;
-        for (Pair pair : commands) {
-            System.out.println(i + ". " + pair.getKey());
-            i++;
+        if (choice) {
+            int i = 1;
+            for (Pair pair : commands) {
+                System.out.println(i + ". " + pair.getKey());
+                i++;
+            }
+            if (previousMenu != null) {
+                System.out.println("0. Назад");
+            } else if (exit) {
+                System.out.println("0. Выход");
+            }
+            System.out.println("Выберите значение...");
         }
-        System.out.println("Выберите значение...");
         read();
     }
 
-    private void read(){
+    private void read() {
         Integer readableNumber = consoleReader.readConsoleLine(this);
+        if (!isChoice()){
+            InputValues.add(readableNumber);
+        }
         CommandFactory.getInstance().create(this, readableNumber).execute();
     }
 
@@ -35,5 +50,32 @@ public class Menu {
 
     public List<Pair<String, CommandType>> getCommands() {
         return commands;
+    }
+
+    public boolean isChoice() {
+        return choice;
+    }
+
+    public Menu setChoice(boolean choice) {
+        this.choice = choice;
+        return this;
+    }
+
+    public Menu setPreviousMenu(Menu menu) {
+        previousMenu = menu;
+        return this;
+    }
+
+    public Menu setExit (boolean bool) {
+        exit = bool;
+        return this;
+    }
+
+    public Menu getPreviousMenu() {
+        return previousMenu;
+    }
+
+    public boolean isExit() {
+        return exit;
     }
 }
--- src/main/java/com/netcracker/homework1/menu/MenuType.java
+++ src/main/java/com/netcracker/homework1/menu/MenuType.java
@@ -4,26 +4,43 @@ package com.netcracker.homework1.menu;
 import com.netcracker.homework1.command.factory.CommandType;
 
 public enum MenuType {
-    START, OPERATIONS, SEARCH, PRINT;
+    START, OPERATIONS, SEARCH, PRINT, CALCULATE, INPUTNUMBER;
 
     public Menu getCurrentMenu() {
         switch (this) {
             case START:
                 return new Menu()
                         .addCommand("Операции", CommandType.OPERATIONS)
-                        .addCommand("Печать", CommandType.PRINT);
-            case OPERATIONS:
+                        .addCommand("Печать", CommandType.PRINT)
+                        .setExit(true);
+            case CALCULATE:
                 return new Menu()
                         .addCommand("Найти максимум", CommandType.FINDMAX)
                         .addCommand("Найти минимум", CommandType.FINDMIN)
                         .addCommand("Найти количество", CommandType.FINDCOUNT)
                         .addCommand("Найти сумму значений", CommandType.FINDSUM)
-                        .addCommand("Найти среднее", CommandType.FINDAVERAGE);
+                        .addCommand("Найти среднее", CommandType.FINDAVERAGE)
+                        .setPreviousMenu(MenuFactory.getInstance().create(MenuType.OPERATIONS));
+            case OPERATIONS:
+                return new Menu()
+                        .addCommand("Вычисления значений", CommandType.CALCULATE)
+                        .addCommand("Поиск", CommandType.SEARCH)
+                        .setPreviousMenu(MenuFactory.getInstance().create(MenuType.START));
             case PRINT:
                 return new Menu()
                         .addCommand("Печать массива в прямом порядке", CommandType.PRINTARRAY)
                         .addCommand("Печать массива в обратном порядке", CommandType.PRINTREVERSEARRAY)
-                        .addCommand("Печать массива в отсортированном порядке", CommandType.PRINTSORTEDARRAY);
+                        .addCommand("Печать массива в отсортированном порядке", CommandType.PRINTSORTEDARRAY)
+                        .setPreviousMenu(MenuFactory.getInstance().create(MenuType.START));
+            case SEARCH:
+                return new Menu()
+                        .addCommand("Проверить входит ли число в массив", CommandType.CHECKNUMBERINARRAY)
+                        .addCommand("Заменить элемент массива на другой", CommandType.UPDATENUBERINARRAY)
+                        .setPreviousMenu(MenuFactory.getInstance().create(MenuType.OPERATIONS));
+            case INPUTNUMBER:
+                return new Menu()
+                        .addCommand("", CommandType.INPUTNUMBERCOMMAND)
+                        .setChoice(false);
             default:
                 return new Menu();
         }
