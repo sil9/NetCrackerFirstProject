860b3e586f532ae13332bd59d9b449ad3e842d79
sil9 <sil9tyt@gmail.com>
2017-10-11 19:08:30 +0300

    update command (finally)

--- src/main/java/com/netcracker/homework1/command/impl/search/UpdateCommand.java
+++ src/main/java/com/netcracker/homework1/command/impl/search/UpdateCommand.java
@@ -11,32 +11,46 @@ public class UpdateCommand implements Command {
 
     @Override
     public void execute() {
-        System.out.println("Введите число которое хотите заменить");
-        MenuFactory.getInstance().create(MenuType.INPUTNUMBER).show();
-        int inputNumber = InputValues.getList().get(InputValues.getList().size() - 1);
-        int[] array = ArrayOfNumbers.getArray();
-        int position = 0;
-        boolean check = false;
-        for (int i = 0 ; i < array.length ; i++) {
-            if (array[i] == inputNumber) {
-                check = true;
-                position = i;
-                break;
-            }
-        }
-        if (!check) {
+        System.out.println("Введите число которое хотите заменить...");
+        int putNumber = readIntFromConsole();
+        int position = checkNumberInArray(putNumber);
+        if (position == -1) {
             System.out.println("Такого числа нету");
             System.out.println("-------");
             MenuFactory.getInstance().create(MenuType.SEARCH).show();
         } else {
-            System.out.println("Введите число на которое заменить");
-            MenuFactory.getInstance().create(MenuType.INPUTNUMBER).show();
-            int replaceableNumber = InputValues.getList().get(InputValues.getList().size() - 1);
-            array[position] = replaceableNumber;
-            System.out.println("Операция прошла успешно");
-            System.out.println("-------");
-            MenuFactory.getInstance().create(MenuType.SEARCH).show();
+            System.out.println("Введите число на которое хотите заменить");
+            int replaceNumber = readIntFromConsole();
+            int result = checkNumberInArray(replaceNumber);
+            if (result != -1) {
+                System.out.println("Такое число уже есть в массиве. (должен оставаться уникальным)");
+                System.out.println("-------");
+                MenuFactory.getInstance().create(MenuType.SEARCH).show();
+            } else {
+                ArrayOfNumbers.getArray()[position] = replaceNumber;
+                System.out.println("Операция прошла успешно");
+                System.out.println("-------");
+                MenuFactory.getInstance().create(MenuType.SEARCH).show();
+            }
         }
-        InputValues.getList().clear();
     }
+
+    private int readIntFromConsole() {
+        MenuFactory.getInstance().create(MenuType.INPUTNUMBER).show();
+        int num = InputValues.getList().get(InputValues.getList().size() - 1);
+        InputValues.getList().clear();
+        return num;
+    }
+
+    private int checkNumberInArray(int number) {
+        int position = -1;
+        for (int i = 0; i < ArrayOfNumbers.getArray().length; i++) {
+            if (number == ArrayOfNumbers.getArray()[i]) {
+                position = i;
+            }
+        }
+        return position;
+    }
+
 }
+
