68e24e1e80b6bc19f50ce901bfdd4bfae1d93db3
sil9 <sil9tyt@gmail.com>
2017-10-11 03:02:44 +0300

    runner

--- src/main/java/com/netcracker/homework1/Main.java
+++ src/main/java/com/netcracker/homework1/Main.java
@@ -1,12 +1,13 @@
 package com.netcracker.homework1;
 
 
+import com.netcracker.homework1.initialize.InitArray;
 import com.netcracker.homework1.menu.MenuFactory;
 import com.netcracker.homework1.menu.MenuType;
 
-
 public class Main {
     public static void main(String[] args) {
+        InitArray.init();
         MenuFactory.getInstance().create(MenuType.START).show();
     }
 }
