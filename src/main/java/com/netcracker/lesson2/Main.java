4cbfc81781a31283ea982e98bbf75e68e9954675
sil9 <sil9tyt@gmail.com>
2017-10-16 18:57:43 +0300

    psvm for lesson

---
+++ src/main/java/com/netcracker/lesson2/Main.java
@@ -0,0 +1,12 @@
+package com.netcracker.lesson2;
+
+
+import com.netcracker.lesson2.string.StringSetTest;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Testable testable = new StringSetTest();
+        testable.test();
+    }
+}
