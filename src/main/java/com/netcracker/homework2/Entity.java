c7a63eca089a2783f724a57a606a5e76f061bf0e
sil9 <sil9tyt@gmail.com>
2017-10-17 21:43:59 +0300

    add entity for second homework

---
+++ src/main/java/com/netcracker/homework2/Entity.java
@@ -0,0 +1,57 @@
+package com.netcracker.homework2;
+
+public class Entity {
+    private String name;
+    private int value;
+
+    public Entity() {
+    }
+
+    public Entity(String name, int value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Entity entity = (Entity) o;
+
+        if (value != entity.value) return false;
+        return name != null ? name.equals(entity.name) : entity.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + (int) (value ^ (value >>> 32));
+        return result;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("Entity{");
+        sb.append("name='").append(name).append('\'');
+        sb.append(", value=").append(value);
+        sb.append('}');
+        return sb.toString();
+    }
+}
